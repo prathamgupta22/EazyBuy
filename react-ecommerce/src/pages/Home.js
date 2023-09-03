b2f9d3a2297cab335a6176fa0d5306e743f2534e
prathamgupta22 <pratham.guptapg01@gmail.com>
2023-09-03 23:47:06 +0530

    structure completed

---
+++ react-ecommerce/src/pages/Home.js
@@ -0,0 +1,13 @@
+import ProductList from "../features/product-list/ProductList";
+import Navbar from "../features/navbar/Navbar";
+function Home() {
+  return (
+    <div>
+      <Navbar>
+        <ProductList></ProductList>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Home;
