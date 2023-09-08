8291a7496cace9e377157ab77fddb2051dd69b56
prathamgupta22 <pratham.guptapg01@gmail.com>
2023-09-09 00:17:07 +0530

    initialised

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Counter } from "./features/counter/Counter";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
