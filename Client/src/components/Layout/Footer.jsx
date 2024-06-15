86e6f9349c1bc5f76be536b08b6f7cb9b14d3c0e
prathamgupta22 <pratham.guptapg01@gmail.com>
2024-06-16 00:47:09 +0530

    started frontend

---
+++ Client/src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-900 text-white p-3">
+      <h4 className="text-center">All Rights Reserved &copy; PG</h4>
+    </div>
+  );
+};
+
+export default Footer;
